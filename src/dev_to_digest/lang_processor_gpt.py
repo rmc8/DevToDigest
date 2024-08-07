74eda3c2f09c6048770031d660ea9cf101d2d8b4
rmc-8.com <kmprmc@gmail.com>
2024-08-07 14:25:37 +0900

    feat: Implement LangProcGpt class and fil_articles function
    - Added LangProcGpt class to handle processing of articles using GPT-4o-mini model
    - Added fil_articles function to filter articles based on reaction count threshold
    - Updated main.py to use LangProcGpt and fil_articles functions

---
+++ src/dev_to_digest/lang_processor_gpt.py
@@ -0,0 +1,113 @@
+from typing import List
+from pydantic import BaseModel, Field
+
+from langchain_openai import ChatOpenAI
+from langchain.prompts import PromptTemplate
+from langchain.output_parsers import PydanticOutputParser, OutputFixingParser
+
+
+class ProcResult(BaseModel):
+    ja_title: str = Field(description="Japanese translation of the article title")
+    ja_summary: str = Field(description="Japanese translation of the summary")
+
+
+SUMMARIZE_PROMPT_TEMPLATE = """
+## Instructions
+
+Please summarize the following English text into a narrative of {summary_sentences} sentences in plain text format.
+
+## Text
+
+================================================================
+
+{contents}
+
+================================================================
+""".strip()
+
+JA_TRANSLATE_PROMPT_TEMPLATE = """
+## Instructions
+Please translate the following text into Japanese.
+
+## Text
+{text}
+""".strip()
+
+PARSE_PROMPT_TEMPLATE = """
+## Instructions
+Please return an object containing a Japanese title and a Japanese summary.
+
+## Input
+### Japanese Title
+{ja_title}
+
+### Japanese Summary
+{ja_summary}
+
+## Output Format
+{formatted_instructions}
+""".strip()
+
+
+class LangProcGpt:
+    MODEL_NAME = "gpt-4o-mini"
+
+    def __init__(
+        self, title: str, contents: str, url: str, tag_list: List[str], api_key: str
+    ):
+        self.title = title
+        self.contents = contents
+        self.url = url
+        self.tag_list = tag_list
+        self.llm = ChatOpenAI(
+            model_name=self.MODEL_NAME, temperature=0, api_key=api_key
+        )
+        parser = PydanticOutputParser(pydantic_object=ProcResult)
+        self.result_parser = OutputFixingParser.from_llm(
+            parser=parser,
+            llm=self.llm,
+        )
+
+    def summarize(self, summary_sentences: int):
+        prompt = PromptTemplate(
+            template=SUMMARIZE_PROMPT_TEMPLATE,
+            input_variables=["summary_sentences", "contents"],
+        )
+        formatted_prompt = prompt.format(
+            summary_sentences=summary_sentences, contents=self.contents
+        )
+        output = self.llm.invoke(formatted_prompt)
+        return output.content
+
+    def translate(self, text: str):
+        prompt = PromptTemplate(
+            template=JA_TRANSLATE_PROMPT_TEMPLATE,
+            input_variables=["text"],
+        )
+        formatted_prompt = prompt.format(text=text)
+        output = self.llm.invoke(formatted_prompt)
+        return output.content
+
+    def summarize_and_translate(self, summary_sentences: int):
+        summary = self.summarize(summary_sentences)
+        ja_summary = self.translate(summary)
+        ja_title = self.translate(self.title)
+        formatted_instructions = self.result_parser.get_format_instructions()
+        prompt = PromptTemplate(
+            template=PARSE_PROMPT_TEMPLATE,
+            input_variables=["ja_title", "ja_summary"],
+            partial_variables={"formatted_instructions": formatted_instructions},
+        )
+        formatted_prompts = prompt.format(ja_title=ja_title, ja_summary=ja_summary)
+        output = self.llm.invoke(formatted_prompts)
+        return self.result_parser.parse(output.content)
+
+    def run(self, summary_sentences: int = 5) -> dict:
+        result = self.summarize_and_translate(summary_sentences)
+        return {
+            "en_title": self.title,
+            "ja_title": result.ja_title,
+            "url": self.url,
+            "tags": ", ".join([f"#{t}" for t in self.tag_list]),
+            "ja_summary": result.ja_summary,
+        }
--- src/main.py
+++ src/main.py
@@ -1,33 +1,62 @@
 import os
+import json
 from typing import List
+
 import fire
 from dotenv import load_dotenv
 from devtopy import DevTo
-from devtopy.model import PublishedArticleList, PublishedArticle
+from devtopy.model import PublishedArticleList, PublishedArticle, ErrorResponse
 
 from dev_to_digest.db import DataBase
 
+from dev_to_digest.lang_processor_gpt import LangProcGpt
 
 THIS_DIR = os.path.abspath(os.path.dirname(__file__))
 DOT_ENV_PATH = os.path.join(THIS_DIR, ".env")
+JSON_PATH = os.path.join(THIS_DIR, "config.json")
+
 load_dotenv(dotenv_path=DOT_ENV_PATH)
+
+with open(JSON_PATH, "r") as f:
+    conf = json.load(f)
 
 
 def fil_articles(
     articles: PublishedArticleList, threshold: int
 ) -> List[PublishedArticle]:
-    return [a for a in articles if a.positive_reactions_count > threshold]
+    return [a for a in articles.articles if a.positive_reactions_count > threshold]
 
 
-def proc(tag_name: str, webhook_url: str, reaction_threshold: int = 20):
+def proc(tag_name: str, webhook_url: str, reaction_threshold: int = 55):
+    # 8% = 20
+    # 5% = 22
+    # 2% = 53
     db = DataBase(dir_path=THIS_DIR)
     dt = DevTo(api_key=os.getenv("DEVTO_API_KEY"))
     res = dt.articles.get(page=1, per_page=1000, tag=tag_name.lower())
     articles = fil_articles(res, reaction_threshold)
     for article in articles:
-        if db.url_exists(article.url):
+        url = str(article.url)
+        if db.url_exists(url):
             continue
-        
+        article_id = article.id
+        detail = dt.articles.get_by_id(article_id)
+        if type(detail) is ErrorResponse:
+            continue
+        title = article.title
+        tags = article.tag_list
+        contents = detail.body_markdown
+        params = {
+            "title": title,
+            "contents": contents,
+            "url": url,
+            "tag_list": tags,
+            "api_key": os.getenv("OPENAI_API_KEY"),
+        }
+        lp = LangProcGpt(**params)
+        data = lp.run()
+        print(data)
+        break
 
 
 def main():
